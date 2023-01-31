cc8cb451a3768a49ac47ca3c03840c88336ea8a0
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-01-31 09:41:07 +0100

    Add project files.

---
+++ K22O/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using K22O;
+
+const int indulokSzama = 691;
+
+List<Versenyzo> versenyzok = new();
+
+using StreamReader sr = new(@"..\..\..\RES\bukkm2019.txt");
+_ = sr.ReadLine();
+while (!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()!));
+
+double f4 = 100 - (versenyzok.Count / (double)indulokSzama * 100);
+Console.WriteLine($"4. feladat: Versenytávot nem teljesítők: {f4}%");
+
+int f5 = versenyzok.Count(x => !x.Nem & x.Tav == "Rövid");
+Console.WriteLine($"5. feladat: Női versenyzők száma a rövidtávú vers3enayen: {f5} fő");
+
+bool f6 = versenyzok.Any(x => x.Ido.TotalHours > 6);
+Console.WriteLine($"6. feladat: {(f6 ? "Volt": "Nem volt")} ilyen versenyző");
+
+Versenyzo f7 = versenyzok
+    .Where(x => x.Kategoria == "ff" & x.Tav == "Rövid")
+    .OrderBy(x => x.Ido)
+    .First();
+Console.WriteLine("7. feladat: a felnőtt férfi (ff) kategória győztese rövid távon:\n" +
+    $"\tRajtszám: {f7.Rajtszam}\n" +
+    $"\tNév: {f7.Nev}\n" +
+    (string.IsNullOrWhiteSpace(f7.Egyesulet) ? string.Empty : $"\tEgyesület: {f7.Egyesulet}\n") +
+    $"\tIdő: {f7.Ido}");
+    
---
+++ K22O/Versenyzo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K22O
+{
+    internal class Versenyzo
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public bool Nem => Kategoria[^1] == 'f';
+        public string Nev { get; set; }
+        public string? Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+        public string Tav => Rajtszam[0] switch
+        {
+            'M' => "Mini",
+            'R' => "Rövid",
+            'K' => "Közép",
+            'H' => "Hosszú",
+            'E' => "Pedelec",
+            _ => throw new Exception("Hibás rajtszám!")
+        };
+
+        public Versenyzo(string sor)
+        {
+            string[] v = sor.Split(';');
+            Rajtszam = v[0];
+            Kategoria = v[1];
+            Nev = v[2];
+            Egyesulet = v[3];
+            Ido = TimeSpan.Parse(v[4]);
+        }
+    }
+}
